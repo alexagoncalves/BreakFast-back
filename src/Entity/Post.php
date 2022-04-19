8ca56eabc8f965c0b2251ee6c7bd2dd404f85291
AnthonyOuzhene <a.ouzhene@gmail.com>
2022-04-19 17:06:38 +0200

    update length for post title

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -25,7 +25,7 @@ class Post
      * @ORM\Column(type="string", length=255)
      * @Groups ({"get_posts_list", "get_post_by_id"})
      * @Assert\NotBlank
-     * @Assert\Length(min = 10, minMessage = "Il faut au minimum {{ limit }} caractères")
+     * @Assert\Length(min = 5, minMessage = "Il faut au minimum {{ limit }} caractères")
      */
     private $title;
 
